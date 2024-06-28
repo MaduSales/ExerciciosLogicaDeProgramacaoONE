b53ec9fc89656a91d02bfb8bc6225954482885ae
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-27 21:41:38 -0300

    Add files via upload

---
+++ exercicio04/script.js
@@ -0,0 +1,5 @@
+//Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+let valor = prompt("Digite o saldo da sua conta");
+
+alert(`O valor do saldo da sua conta é ${valor}`); // <- Isso é Template String
